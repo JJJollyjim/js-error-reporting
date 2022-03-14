7ae02994fbc5067998678c9931f2bed4e5c3f6c1
Jamie McClymont <jamie@kwiius.com>
2022-03-15 00:36:46 +1300

    maybe

--- default.nix
+++ default.nix
@@ -1,7 +1,8 @@
 let
   sources = import nix/sources.nix;
 in
-{ rustChannel ? ((pkgs.extend (import "${sources.nixpkgs-mozilla}/rust-overlay.nix")).latest.rustChannels.nightly), pkgs ? import sources.nixpkgs {} }:
+#{ rustChannel ? ((pkgs.extend (import "${sources.nixpkgs-mozilla}/rust-overlay.nix")).latest.rustChannels.nightly), pkgs ? import sources.nixpkgs {} }:
+{ pkgs }:
 with pkgs;
 with lib;
 rec {
@@ -16,7 +17,8 @@ rec {
     dontInstall = true;
   };
 
-  server = ((pkgs.extend (super: self: { rustc = rustChannel.rust; inherit (rustChannel) cargo rust rust-fmt rust-std clippy; })).callPackage ./server/Cargo.nix {}).rootCrate.build.override {
+  server = (pkgs.callPackage ./server/Cargo.nix {}).rootCrate.build.override {
+  #server = ((pkgs.extend (super: self: { rustc = rustChannel.rust; inherit (rustChannel) cargo rust rust-fmt rust-std clippy; })).callPackage ./server/Cargo.nix {}).rootCrate.build.override {
     runTests = true;
   };
 
